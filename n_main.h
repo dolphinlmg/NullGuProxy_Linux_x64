423fbbb0c82e3a8fa020f1a0ef6212db96c62b51
dolphinlmg <dolphinlmg@gmail.com>
2019-10-24 01:15:11 +0900

    Add functions to n_main.h

--- n_main.h
+++ n_main.h
@@ -1,20 +1,27 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef N_MAIN_H
+#define N_MAIN_H
 #include <iostream>
 #include <fstream>
-#include <vector>
-#include "Class/n_Pcap/n_pcap_data.h"
+#include <signal.h>
 #include "Class/Packet/n_tcp.h"
 #include "Class/n_Pcap/n_pcap.h"
-#include "Class/Packet/n_packet.h"
+#include "Class/n_Pcap/n_pcap_data.h"
 
 using namespace std;
 
+// namespace for main
 namespace MAIN {
-
     static n_Pcap_Data* file;
     static vector<uint16_t> ports;
     static const char* portFileName = "ports.ng";
+
+    // signal handler for sigint
+    [[ noreturn ]] void handler(int s) {
+        cout << endl << "Signal Captured: " << s
+             << endl << "Exporting to file..." << endl;
+        file->exportToFile();
+        exit(0);
+    }
 
     bool readPortsFromFile() {
         try {
@@ -22,20 +29,32 @@ namespace MAIN {
             if (!is.is_open())
                 return false;
             uint16_t tmp;
-            while (!is.eof()){
+            while(!is.eof()){
                 is >> tmp;
                 if (is.good())
                     ports.push_back(tmp);
             }
             is.close();
         } catch (exception& ex) {
-            cerr << ex.what() << endl;
-            return false;
+           cerr << ex.what() << endl;
+           return false;
         }
-        return true;
+       return true;
     }
+
+    void init() {
+        // read ports from 'ports.ng'
+        if (!readPortsFromFile()) {
+            cerr << "Error to read port file!" << endl;
+            exit(-1);
+        }
+
+        // register signal handler
+        signal(SIGINT, handler);
+    }
+
 }
 
 using namespace MAIN;
 
-#endif // MAIN_H
+#endif // N_MAIN_H
